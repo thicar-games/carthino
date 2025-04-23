82be04b6498e1455c3a9063e85fae863fd4aabc7
carlos-salm <carlos.es01@aluno.ifsc.edu.br>
2025-04-23 18:24:14 +0000

    Criar Jogo Mínimo

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { Game, Types } from "phaser"
+
+const config: Types.Core.GameConfig = {
+type: Phaser .AUTO,
+width: 800,
+height: 450,
+parent: "game-container"
+}
+
+export default new Game(config)
+
+
